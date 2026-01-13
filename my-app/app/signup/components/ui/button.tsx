37497bd391707d3cd715518c7c25c030ee8e7235
cheung cheuk man chevy <chevy@cheungs-MacBook-Pro-2.local>
2026-01-13 01:32:57 +0100

    Resolve button.tsx merge conflict

--- my-app/app/signup/components/ui/button.tsx
+++ my-app/app/signup/components/ui/button.tsx
@@ -5,19 +5,11 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-<<<<<<< HEAD
   "inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
   {
     variants: {
       variant: {
         default: "rounded-sm bg-green-500 text-white hover:bg-green-300 hover:text-green-950 disabled:opacity-50 disabled:cursor-not-allowed",
-=======
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
-  {
-    variants: {
-      variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
->>>>>>> 7a9937d14e3d55973890e4611c6b3831970b0921
         destructive:
           "bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
@@ -29,11 +21,7 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-<<<<<<< HEAD
         default: "h-10 px-3 py-2 has-[>svg]:px-2.5",
-=======
-        default: "h-9 px-4 py-2 has-[>svg]:px-3",
->>>>>>> 7a9937d14e3d55973890e4611c6b3831970b0921
         sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
         lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
         icon: "size-9",
