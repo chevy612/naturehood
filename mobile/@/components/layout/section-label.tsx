ce2a89dff8b23084a1ff9efe34006e11b09dac11
cheung cheuk man chevy <chevy@cheungs-MacBook-Pro-2.local>
2026-04-04 20:17:49 +0200

    chore: extract mobile app to standalone repo (naturehood-mobile)

--- mobile/@/components/layout/section-label.tsx
+++
@@ -1,9 +0,0 @@
-import { Text } from '@/components/ui/text';
-
-type Props = {
-  text: string;
-};
-
-export function SectionLabel({ text }: Props) {
-  return <Text variant="small">{text.toUpperCase()}</Text>;
-}
--- mobile/index.js
+++
@@ -1,8 +0,0 @@
-import { registerRootComponent } from 'expo';
-
-if (process.env.EXPO_PUBLIC_STORYBOOK === 'true') {
-  const StorybookUIRoot = require('./.storybook').default;
-  registerRootComponent(StorybookUIRoot);
-} else {
-  require('expo-router/entry');
-}
